1832331878731c05cec8f08618b2dc550deea6f1
Kartheek <kartheek@mwebware.com>
2017-12-14 16:42:48 +0530

    Report Text support for \n and \t

--- src/app/modules/simulator/report-text/report-text.component.ts
+++ src/app/modules/simulator/report-text/report-text.component.ts
@@ -1,28 +1,39 @@
-import { Component, OnInit, Input , EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { ReportText } from '../shared/models/report-text.model';
 
 @Component({
-  selector: 'acr-report-text',
-  templateUrl: './report-text.component.html',
-  styleUrls: ['../../styles.css']
+    selector: 'acr-report-text',
+    templateUrl: './report-text.component.html',
+    styleUrls: ['../../styles.css']
 })
-export class ReportTextComponent  {
+export class ReportTextComponent {
 
-  @Input() FormValues: Object = {};
-  @Input() ReportTexts: ReportText[] = [];
+    @Input() FormValues: Object = {};
+    @Input() ReportTexts: ReportText[] = [];
 
 
-  evaluate(cond) {
-      if (cond === '') {
-          return false;
-      }
-      return eval(cond);
-  }
+    evaluate(cond) {
+        if (cond === '') {
+            return false;
+        }
+        return eval(cond);
+    }
 
-  InsertValue(dataElementID) {
-      if (Array.isArray(this.FormValues[dataElementID])) {
-          return this.FormValues[dataElementID].join(', ');
-      }
-      return this.FormValues[dataElementID];
-  }
+    InsertValue(dataElementID) {
+        if (Array.isArray(this.FormValues[dataElementID])) {
+            return this.FormValues[dataElementID].join(', ');
+        }
+        return this.FormValues[dataElementID];
+    }
+
+    FormatReportText(reportText: string) {
+        if (reportText.indexOf('\\n') !== -1) {
+            reportText = reportText.replace(/\\n/g, '<br />');
+        }
+        if (reportText.indexOf('\\t') !== -1) {
+            reportText = reportText.replace(/\\t/g, '    ');
+        }
+
+        return reportText;
+    }
 }
