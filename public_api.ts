3d8524fe532cf03b44aaff210b3595879e11e0df
ssathasivan <ssathasivan@gmail.com>
2018-02-08 09:01:28 -0500

    Updated the API file

--- public_api.ts
+++ public_api.ts
@@ -1,2 +1,2 @@
-export * from './src/app/modules/simulator/simulator.module';
+export * from './src/app/modules/acr-assist-simulator/acr-assist-simulator.module';
 export * from './src/app/modules/simulatorloader/simulatorloader.module';
