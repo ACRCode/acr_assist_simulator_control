9b7c139765b711049734edd9bdf46a521a3c4aeb
Sreejith <SSreedharan@acr.org>
2020-05-06 19:11:31 +0530

    Code refactorimgs

--- src/app/modules/acr-assist-simulator/assist-data-element/image-map/image-map.component.ts
+++ src/app/modules/acr-assist-simulator/assist-data-element/image-map/image-map.component.ts
@@ -15,7 +15,6 @@ export class ImageMapComponent implements OnInit {
 
   selectionValue = '';
   map_selector_class = 'map-selector';
-  imageExist = true;
   formValues: object = {};
   selectedValues = [];
 
