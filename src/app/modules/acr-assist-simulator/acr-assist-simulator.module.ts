6e6c9483e7c3c98b8c65a0f32420e769bbd5cad9
Sreejith <SSreedharan@acr.org>
2020-06-07 12:55:25 +0530

    updated the package

--- src/app/modules/acr-assist-simulator/acr-assist-simulator.module.ts
+++ src/app/modules/acr-assist-simulator/acr-assist-simulator.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { CoreModule } from '../core/core.module';
+import { DragScrollModule } from 'ngx-drag-scroll';
+import { NgxLoadingModule } from 'ngx-loading';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 import { AcrAssistSimulatorComponent } from './acr-assist-simulator/acr-assist-simulator.component';
 import { AssistDataElementComponent } from './assist-data-element/assist-data-element.component';
 import { HintDiagramComponent } from './assist-data-element/hint-diagram/hint-diagram.component';
@@ -9,35 +17,30 @@ import { AssistChoiceElementComponent } from './assist-data-element/assist-choic
 import { AssistMultiChoiceElementComponent } from './assist-data-element/assist-multi-choice-element/assist-multi-choice-element.component';
 import { ImageMapComponent } from './assist-data-element/image-map/image-map.component';
 import { AssistReportTextComponent } from './assist-report-text/assist-report-text.component';
-import { SimulatorEngineService } from '../core/services/simulator-engine.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AssistDurationElementComponent } from './assist-data-element/assist-duration-element/assist-duration-element.component';
+import { AssistDateTimeElementComponent } from './assist-data-element/assist-date-time-element/assist-date-time-element.component';
+
+import { ClipboardDirective } from './shared/directives/clipboard.directive';
 import { DynamicHeightDirective } from './shared/directives/dynamicheight.directive';
 import { NumericOnlyDirective } from './shared/directives/numeric-only.directive';
-import { DragScrollModule } from 'ngx-drag-scroll';
-import { NgxLoadingModule } from 'ngx-loading';
-import { AssistDurationElementComponent } from './assist-data-element/assist-duration-element/assist-duration-element.component';
+import { IntegerMaxRestrictDirective } from './shared/directives/integer-restrict-value-greaterthan-max.directive';
+import { SecurePipe } from './shared/pipes/secure.pipe';
+
+import { SimulatorEngineService } from '../core/services/simulator-engine.service';
 import { SimulatorCommunicationService } from './shared/services/simulator-communication.service';
 import { ResetCommunicationService } from './shared/services/reset-communication.service';
-import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
-import { AssistDateTimeElementComponent } from './assist-data-element/assist-date-time-element/assist-date-time-element.component';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { IntegerMaxRestrictDirective } from './shared/directives/integer-restrict-value-greaterthan-max.directive';
-import { ClipboardDirective } from './shared/directives/clipboard.directive';
 import { ClipboardService } from './shared/services/clipboard.service';
-import { SecurePipe } from './shared/pipes/secure.pipe';
 import { UrlHelperService } from './shared/services/url-helper.service';
 
-const components = [
-  AcrAssistSimulatorComponent, AssistDataElementComponent, HintDiagramComponent,
+const components = [AcrAssistSimulatorComponent, AssistDataElementComponent, HintDiagramComponent,
   AssistNumericElementComponent, AssistChoiceElementComponent, AssistMultiChoiceElementComponent,
   ImageMapComponent, AssistReportTextComponent, DynamicHeightDirective, NumericOnlyDirective, SecurePipe,
   IntegerMaxRestrictDirective, ClipboardDirective, AssistDateTimeElementComponent, AssistDurationElementComponent];
-const modules = [
-  CommonModule, CoreModule, FormsModule, CarouselModule.forRoot(), ModalModule.forRoot(), ReactiveFormsModule,
+
+const modules = [CommonModule, CoreModule, FormsModule, CarouselModule.forRoot(), ModalModule.forRoot(), ReactiveFormsModule,
   DragScrollModule, OwlDateTimeModule, OwlNativeDateTimeModule, NgxLoadingModule];
-const services = [
-  SimulatorEngineService, SimulatorCommunicationService, ResetCommunicationService, ClipboardService, UrlHelperService];
+
+const services = [SimulatorEngineService, SimulatorCommunicationService, ResetCommunicationService, ClipboardService, UrlHelperService];
 
 @NgModule({
   imports: modules,
