a78a7f3695bc904cd7e6633036cc69c616c0a518
Karthika <kudayasankar@acr.org>
2022-02-18 19:09:17 +0530

    Revert "Checkmarx issue with imagemap url"
    This reverts commit 94b375eb9a44aae1a3eef5186b530d1d28d371e9.

--- src/app/modules/acr-assist-simulator/shared/pipes/sanitizer-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SanitizerPipePipe } from './sanitizer.pipe';
-
-describe('SanitizerPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SanitizerPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
